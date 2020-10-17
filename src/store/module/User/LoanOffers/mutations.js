d031e7554c764e08119c692a7f62b75f4e61fab4
wesley <wesleyukadike@yahoo.com>
2020-10-17 21:47:34 +0100

    added loader for loan offers

--- src/store/module/User/LoanOffers/mutations.js
+++ src/store/module/User/LoanOffers/mutations.js
@@ -1,9 +1,7 @@
-export const FETCH_LOAN_OFFERS_SUCCESS=(state,data)=>{
-    state.loanOffers=data
-}
+export const FETCH_LOAN_OFFERS_SUCCESS = (state, data) => {
+  state.loanOffers = data;
+};
 
-export const FETCHING_LOAN_OFFERS=(state,status)=>{
-    
-    state.fetchingLoanOffers=status
-    console.log(state.fetchingLoanOffers);
-}
+export const FETCHING_LOAN_OFFERS = (state, status) => {
+  state.fetchingLoanOffers = status;
+};
