2ec67afc85cb15fad225c462ed48acb3497c9eb1
wesley <wesleyukadike@yahoo.com>
2020-11-21 07:24:38 +0100

    changed logo and wrapper in left hand menu

--- src/components/Layout/AdminMain.vue
+++ src/components/Layout/AdminMain.vue
@@ -1,56 +1,62 @@
 <template>
-     <div class="main">
-        <div class="left">
-            <img @click="$router.push({name:'adminDashboard'})" src="/assets/images/WACS.png" class="logo" alt="WACS logo">
-            <LeftMenuItems />
-        </div>
-        <div class="right">
-            <Header
-              :details="$route.meta"
-              :entityName="companyName"
-              :userName="fullName"
-              logoutRoute='Adminlogin'
-              notificationRoute='AdminNotifications'
-            />
-            <div class="main-body">
-                <LoanManagementMenu v-if="!$route.meta.parents && currentNameSpace === 'loan'"/>
-                <AdminUserManagementMenu v-if="!$route.meta.parents && currentNameSpace === 'users'"/>
-                <div class="main-body-content">
-                    <router-view />
-                </div>
-            </div>
-        </div>
+  <div class="main">
+    <div class="left">
+      <div class="menu-logo-wrapper">
+        <img src="/assets/images/WACS.png" alt="logo" />
+        <p>
+          WORKERS AGGREGATED
+          CREDIT SCHEME
+        </p>
+      </div>
+      <LeftMenuItems />
     </div>
+    <div class="right">
+      <Header
+        :details="$route.meta"
+        :entityName="companyName"
+        :userName="fullName"
+        logoutRoute="Adminlogin"
+        notificationRoute="AdminNotifications"
+      />
+      <div class="main-body">
+        <LoanManagementMenu v-if="!$route.meta.parents && currentNameSpace === 'loan'" />
+        <AdminUserManagementMenu v-if="!$route.meta.parents && currentNameSpace === 'users'" />
+        <div class="main-body-content">
+          <router-view />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-    import Header from '../Header';
-    import LeftMenuItems from '../Admin/AdminLeftMenuItems';
-    import LoanManagementMenu from '../Admin/AdminLoanManagementMenu';
-    import AdminUserManagementMenu from '../Admin/AdminUserManagementMenu'
-    export default {
-        components: {
-            Header,
-            LeftMenuItems,
-            LoanManagementMenu,
-            AdminUserManagementMenu
-        },
-        computed: {
-            fullName() {
-                if(this.$store.getters['Auth/authenticated']) {
-                    return this.$store.state.Auth.user.data.full_name;
-                }
-                return '';
-            },
-            companyName() {
-                if(this.$store.getters['Auth/authenticated']) {
-                    return this.$store.state.Auth.user.data.profile.company.name;
-                }
-                return '';
-            },
-            currentNameSpace() {
-                return this.$route.meta.nameSpace;
-            },
-        },
+import Header from "../Header";
+import LeftMenuItems from "../Admin/AdminLeftMenuItems";
+import LoanManagementMenu from "../Admin/AdminLoanManagementMenu";
+import AdminUserManagementMenu from "../Admin/AdminUserManagementMenu";
+export default {
+  components: {
+    Header,
+    LeftMenuItems,
+    LoanManagementMenu,
+    AdminUserManagementMenu
+  },
+  computed: {
+    fullName() {
+      if (this.$store.getters["Auth/authenticated"]) {
+        return this.$store.state.Auth.user.data.full_name;
+      }
+      return "";
+    },
+    companyName() {
+      if (this.$store.getters["Auth/authenticated"]) {
+        return this.$store.state.Auth.user.data.profile.company.name;
+      }
+      return "";
+    },
+    currentNameSpace() {
+      return this.$route.meta.nameSpace;
     }
+  }
+};
 </script>
--- src/components/Layout/IppisMain.vue
+++ src/components/Layout/IppisMain.vue
@@ -1,53 +1,59 @@
 <template>
-     <div class="main">
-        <div class="left">
-            <img @click="$router.push({name:'ippisLoanRequest'})" src="/assets/images/WACS.png" class="logo" alt="WACS logo">
-            <LeftMenuItems />
-        </div>
-        <div class="right">
-            <Header
-              :details="$route.meta"
-              :entityName="companyName"
-              :userName="fullName"
-              logoutRoute='ippisLogin'
-              notificationRoute='ippisNotifications'
-            />
-            <div class="main-body">
-                <!-- <LoanManagementMenu v-if="!$route.meta.parents && currentNameSpace === 'loan'"/> -->
-                <div class="main-body-content">
-                    <router-view />
-                </div>
-            </div>
-        </div>
+  <div class="main">
+    <div class="left">
+      <div class="menu-logo-wrapper">
+        <img src="/assets/images/WACS.png" alt="logo" />
+        <p>
+          WORKERS AGGREGATED
+          CREDIT SCHEME
+        </p>
+      </div>
+      <LeftMenuItems />
     </div>
+    <div class="right">
+      <Header
+        :details="$route.meta"
+        :entityName="companyName"
+        :userName="fullName"
+        logoutRoute="ippisLogin"
+        notificationRoute="ippisNotifications"
+      />
+      <div class="main-body">
+        <!-- <LoanManagementMenu v-if="!$route.meta.parents && currentNameSpace === 'loan'"/> -->
+        <div class="main-body-content">
+          <router-view />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-    import Header from '../Header';
-    import LeftMenuItems from '../Ipiss/IppisLeftMenuItems';
-    //import LoanManagementMenu from '../LoanManagementMenu';
-    export default {
-        components: {
-            Header,
-            LeftMenuItems,
-            //LoanManagementMenu,
-        },
-        computed: {
-            fullName() {
-                if(this.$store.getters['Auth/authenticated']) {
-                    return this.$store.state.Auth.user.data.full_name;
-                }
-                return '';
-            },
-            companyName() {
-                if(this.$store.getters['Auth/authenticated']) {
-                    return this.$store.state.Auth.user.data.roles[0].name;
-                }
-                return '';
-            },
-            currentNameSpace() {
-                return this.$route.meta.nameSpace;
-            },
-        },
+import Header from "../Header";
+import LeftMenuItems from "../Ipiss/IppisLeftMenuItems";
+//import LoanManagementMenu from '../LoanManagementMenu';
+export default {
+  components: {
+    Header,
+    LeftMenuItems
+    //LoanManagementMenu,
+  },
+  computed: {
+    fullName() {
+      if (this.$store.getters["Auth/authenticated"]) {
+        return this.$store.state.Auth.user.data.full_name;
+      }
+      return "";
+    },
+    companyName() {
+      if (this.$store.getters["Auth/authenticated"]) {
+        return this.$store.state.Auth.user.data.roles[0].name;
+      }
+      return "";
+    },
+    currentNameSpace() {
+      return this.$route.meta.nameSpace;
     }
+  }
+};
 </script>
